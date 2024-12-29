35dd0800b7d8bc02fbee17e2271cb9984d9dd2e2
Adeeba Fatima <adeebafatima1@gmail.com>
2024-12-29 08:59:30 +0530

    ep1_SideBar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,10 +1,64 @@
 import React from "react";
-import MenuItems from "./MenuItems";
 
 const SideBar = () => {
   return (
-    <div>
-      <MenuItems />
+    <div className="p-5 w-48 shadow-lg">
+      <ul>
+        <li>Home</li>
+        <li>Shorts</li>
+        <li>Subscriptions</li>
+      </ul>
+
+      <h1 className="font-bold pt-5">You</h1>
+      <ul>
+        <li>History</li>
+        <li>Playlists</li>
+        <li>Your videos</li>
+        <li>Your courses</li>
+        <li>Watch later</li>
+        <li>Liked videos</li>
+      </ul>
+
+      <h1 className="font-bold pt-5">Subscriptions</h1>
+      <ul>
+        {" "}
+        <li>Huda TV</li>
+        <li>Akshay Saini</li>
+        <li>Chirag Goel</li>
+        <li>One Message Foundation</li>
+        <li>Stories of the Prophets</li>
+      </ul>
+
+      <h1 className="font-bold pt-5">Explore</h1>
+      <ul>
+        <li>Trending</li>
+        <li>Shopping</li>
+        <li>Music</li>
+        <li>Movies</li>
+        <li>Live</li>
+        <li>Gaming</li>
+        <li>News</li>
+        <li>Sports</li>
+        <li>Courses</li>
+        <li>Podcasts</li>
+        <li>Fashion & Beauty</li>
+      </ul>
+
+      <h1 className="font-bold pt-5">More from Youtube</h1>
+      <ul>
+        {" "}
+        <li>YouTube Premium</li>
+        <li>YouTube Studio</li>
+        <li>Youtube Music</li>
+        <li>Youtube Kids</li>
+      </ul>
+
+      <ul className="pt-5">
+        <li>Settings</li>
+        <li>Report History</li>
+        <li>Help</li>
+        <li>Send feedback</li>
+      </ul>
     </div>
   );
 };
