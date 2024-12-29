cf5d46af7f1a5e833046bd9c107166724decd6a8
Adeeba Fatima <adeebafatima1@gmail.com>
2024-12-29 08:37:31 +0530

    ep1_Head

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
 
 const Head = () => {
   return (
-    <div>Head</div>
-  )
-}
+    <div className="grid grid-flow-col p-5 shadow-lg">
+      <div className="flex col-span-1">
+        <img
+          className="h-8"
+          alt="menu"
+          src="https://53.fs1.hubspotusercontent-na1.net/hub/53/hubfs/What%20is%20a%20Hamburger%20Button.png?width=450&name=What%20is%20a%20Hamburger%20Button.png"
+        />
+        <img
+          className="h-8 mx-2"
+          alt="youtube-logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Logo_of_YouTube_%282013-2015%29.svg/2560px-Logo_of_YouTube_%282013-2015%29.svg.png"
+        />
+      </div>
+      <div className="col-span-10 px-10">
+        <input
+          type="text"
+          placeholder="   Search"
+          className="w-1/2 border border-gray-400 p-2 rounded-l-full"
+        />
+        <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100">
+          🔍
+        </button>
+      </div>
+      <div className="col-span-1">
+        <img
+          className="h-8"
+          alt="youtube-logo"
+          src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Head
+export default Head;
