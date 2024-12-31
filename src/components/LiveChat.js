c106cd0b1f9cf5d144851162543b23c0cb4ff63a
Adeeba Fatima <adeebafatima1@gmail.com>
2024-12-31 10:35:50 +0530

    ep3_SendMessage_Feature_LiveChat

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ChatMessage from "./ChatMessage";
 import { useDispatch, useSelector } from "react-redux";
 import { addMessage } from "../utils/chatSlice";
-import { generateRandomName,generateRandomMessage } from "../utils/helper";
+import { generateRandomName, generateRandomMessage } from "../utils/helper";
 
 const LiveChat = () => {
+  const [liveMessage, setLiveMessage] = useState("");
   const dispatch = useDispatch();
   const chatMessages = useSelector((store) => store.chat.messages);
   useEffect(() => {
@@ -14,22 +15,45 @@ const LiveChat = () => {
       dispatch(
         addMessage({
           name: generateRandomName(),
-          message: generateRandomMessage(30)
+          message: generateRandomMessage(30),
         })
       );
-    }, 1000);
+    }, 1500);
     return () => clearInterval(i);
   });
   return (
-    <div className="flex flex-col-reverse h-[700px] p-2 border border-black bg-slate-100 rounded-lg w-full overflow-y-scroll">
-      {chatMessages.map((message, index) => (
-        <ChatMessage
-          key={index}
-          name={message.name}
-          message={message.message}
+    <>
+      <div className="flex flex-col-reverse h-[700px] p-2 border border-black bg-slate-100 rounded-lg w-full overflow-y-scroll">
+        {chatMessages.map((message, index) => (
+          <ChatMessage
+            key={index}
+            name={message.name}
+            message={message.message}
+          />
+        ))}
+      </div>
+      <form
+        className="w-full p-2 border border-black rounded-lg"
+        onSubmit={(e) => {
+          e.preventDefault();
+          dispatch(
+            addMessage({
+              name: "Adeeba",
+              message: liveMessage,
+            })
+          );
+          setLiveMessage("");
+        }}
+      >
+        <input
+          type="text"
+          className="border border-gray-400 w-9/12"
+          value={liveMessage}
+          onChange={(e) => setLiveMessage(e.target.value)}
         />
-      ))}
-    </div>
+        <button className="bg-gray-200 rounded-lg ml-2 px-2">Send</button>
+      </form>
+    </>
   );
 };
 
