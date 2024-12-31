78e0242d3ce5c57081fafbdcb01ee8d82130a378
Adeeba Fatima <adeebafatima1@gmail.com>
2024-12-31 10:11:53 +0530

    ep3_LiveChat_functionality

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ChatMessage from "./ChatMessage";
+import { useDispatch, useSelector } from "react-redux";
+import { addMessage } from "../utils/chatSlice";
+import { generateRandomName,generateRandomMessage } from "../utils/helper";
 
 const LiveChat = () => {
+  const dispatch = useDispatch();
+  const chatMessages = useSelector((store) => store.chat.messages);
+  useEffect(() => {
+    //Long Polling/API Polling
+    const i = setInterval(() => {
+      //Data Storage in Redux Store
+      dispatch(
+        addMessage({
+          name: generateRandomName(),
+          message: generateRandomMessage(30)
+        })
+      );
+    }, 1000);
+    return () => clearInterval(i);
+  });
   return (
-    <div className="h-[700px] p-2 border border-black bg-slate-100 rounded-lg w-full">
-      <ChatMessage name="Adeeba" message="God is one" />
-      <ChatMessage name="Adeeba" message="God is one" />
-      <ChatMessage name="Adeeba" message="God is one" />
-      <ChatMessage name="Adeeba" message="God is one" />
+    <div className="flex flex-col-reverse h-[700px] p-2 border border-black bg-slate-100 rounded-lg w-full overflow-y-scroll">
+      {chatMessages.map((message, index) => (
+        <ChatMessage
+          key={index}
+          name={message.name}
+          message={message.message}
+        />
+      ))}
     </div>
   );
 };
